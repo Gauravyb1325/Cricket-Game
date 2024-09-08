385f60020235fd7c05e8a5cfa8325673838a50ef
Gauravyb1325 <gauravyb1325@gmail.com>
2024-09-08 21:03:37 +0530

    object score commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// creating an object to have a scoring of match
+let score = {
+  win : 0,
+  lost : 0,
+  tie : 0,
+};
+
 // Generating Random Computer Choice
 function getComputerChoice(){
   let randomNumber = Math.random() * 3;
@@ -18,30 +25,39 @@ function generateResult(userChoice, computerMsg){
   if(userChoice == 'Bat'){
 
     if(computerMsg === 'Bat'){
+      score.tie++;
       return `It's a tie.`;
     } else if(computerMsg === 'Ball'){
+      score.win++;
       return 'User Won.';
     } else if(computerMsg === 'Stump' ){
+      score.lost++;
       return 'Computer Won.';
     }
 
   } else if(userChoice == 'Ball'){
 
     if(computerMsg === 'Bat'){
+      score.lost++;
       return `Computer Won.`;
     } else if(computerMsg === 'Ball'){
+      score.tie++;
       return `It's a tie`;
     } else if(computerMsg === 'Stump' ){
+      score.win++;
       return `User Won.`;
     }
 
   } else{
 
     if(computerMsg === 'Bat'){
+      score.win++;
       return `User Won.`;
     } else if(computerMsg === 'Ball'){
+      score.lost++;
       return `Computer Won`;
     } else if(computerMsg === 'Stump' ){
+      score.tie++;
       return `It's a tie.`;
     }
 
@@ -52,5 +68,9 @@ function generateResult(userChoice, computerMsg){
 
 // alerting the page to print the result.
 function printMessage(user, computer, result){
-  alert(`You chosen ${user}. Computer chosen ${computer}. ${result}`);
+  alert(`You chosen ${user}. Computer chosen ${computer}.
+     
+${result}
+
+Win : ${score.win}, Lost : ${score.lost}, Tie : ${score.tie}.`);
 }
